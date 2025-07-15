ddecd7ff46c769ea42accf35511d2dbcb09f28f0
adithyan <adithyan.m.r.no12@gmail.com>
2025-07-15 21:18:32 +0530

    Day 8: Modules, packages, and virtual env programs

---
+++ IndustryReadyDay8/use_my_math.py
@@ -0,0 +1,4 @@
+import my_math
+in1=int(input("Enter first number: "))
+in2=int(input("Enter second number: "))
+print("The product is:",my_math.multiply(in1,in2))
---
+++ IndustryReadyDay8/use_package.py
@@ -0,0 +1,5 @@
+from package import basic
+in1 = int(input("Enter first number: "))
+in2 = int(input("Enter second number: "))
+print("The sum is:", basic.add(in1, in2))
+print("The difference is:", basic.sub(in1, in2))
